8076fa461ce939ec1470613ce32cb473e93b4662
Phantom <abracadabra1253@gmail.com>
2024-01-22 17:47:31 +0400

    some changes

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,9 +5,78 @@ import './footer.css';
 export function Footer() {
 	return (<>
 	
+	
+	<div className="footerWlinks">
+	
+	<div>
+		<p className="p">CONTACT</p>
+		
+		<p><i className="fa fa-map-marker-alt me-3"></i> 123 Street, New York, USA</p>
+		<p><i className="fa fa-phone-alt me-3"></i> +012 345 67890</p>
+		<p><i className="fa fa-envelope me-3"></i> info@example.com</p>
+		
+			<div className="ftr1">
+					<a href=""><i className="fab fa-twitter"></i></a>
+					<a href=""><i className="fab fa-facebook-f"></i></a>
+					<a href=""><i className="fab fa-youtube"></i></a>
+					<a href=""><i className="fab fa-linkedin-in"></i></a>	
+				</div>
+		
+		
+	
+	</div>
+	
+	
+	<div>
+	
+		<p className="p">COMPANY</p>
+		
+			
+		<div className="KZl1">
+			<a href="">About us</a>
+			<a href="">Contact us</a>
+			<a href="">Support</a>
+			<a href="">FAQ</a>
+			<a href="">Terms & Conditions</a>
+		</div>
+		
+	
+	
+	</div>
+	
+	
+	
+	<div>
+	
+			<p className="p">SERVICES</p>
+			
+			<div className="KZl1">
+				<a href="">Foods</a>
+				<a href="">Restaurants</a>
+				<a href="">Fitness & SPA</a>
+				<a href="">Events & Parties</a>
+				<a href="">GYM</a>
+			</div>
+		
+	
+	
+	</div>
+	
+	
+	<div></div>	
+	
+	
+	</div>	
+	
+	
+	
+	
+	<div className="Divider">  </div>
+	
+	
 	<div className="footer">
 	
-	<div> © By Phantom</div>
+	<div> © Designed by Phantom</div>
 	
 			<div>
 				<div className="ftr1">
