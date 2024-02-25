d42d1dc090ba52143e78df9b3269c40f44d5a5f1
Phantom <abracadabra1253@gmail.com>
2024-02-25 18:59:33 +0400

    ....

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { Navigation } from '../nav/nav.js';
 import { Footer } from '../footer/footer.js';
+import "./i.css"
 
 
 export function Index_login() { 
